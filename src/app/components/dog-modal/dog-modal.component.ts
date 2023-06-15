13cc723d88527747553860dfcaefd8b245127115
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-06-15 14:50:53 +0200

    push

--- src/app/components/dog-modal/dog-modal.component.ts
+++ src/app/components/dog-modal/dog-modal.component.ts
@@ -5,17 +5,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-dog-modal',
   templateUrl: './dog-modal.component.html',
   styleUrls: ['./dog-modal.component.scss'],
-  animations: [
-    trigger('fadeIn', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('0.3s', style({ opacity: 1 }))
-      ]),
-      transition(':leave', [
-        animate('0.3s', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+  
 })
 export class DogModalComponent implements OnInit {
   @Input() title: string | undefined;
--- src/app/pages/image-viewer/image-viewer.component.ts
+++ src/app/pages/image-viewer/image-viewer.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
-  styleUrls: ['./image-viewer.component.scss']
+  styleUrls: ['./image-viewer.component.css']
 })
-export class ImageViewerComponent implements OnInit {
-  @Input() dog: any;
-  @Input() immagine: string | undefined;
-  constructor() { }
+export class ImageViewerComponent {
+  @ViewChild('image', { static: false }) image: ElementRef;
 
-  ngOnInit() {
+  private scale = 1;
+  private mouseX = 0;
+  private mouseY = 0;
+
+  onMouseMove(event: MouseEvent) {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+    this.updateTransform();
   }
 
+  onWheel(event: WheelEvent) {
+    event.preventDefault();
+    const delta = event.deltaY < 0 ? 1.1 : 0.9;
+    this.scale *= delta;
+    this.updateTransform();
+  }
+
+  private updateTransform() {
+    const transform = `scale(${this.scale}) translate(${this.mouseX * (1 - this.scale)}px, ${this.mouseY * (1 - this.scale)}px)`;
+    this.image.nativeElement.style.transform = transform;
+  }
 }
