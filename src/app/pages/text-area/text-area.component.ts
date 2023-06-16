55fb15c644e43e050baf004b451c1a3b90c41156
KuiilMcFly <carmine.esp40@gmail.com>
2023-06-16 15:52:02 +0200

    tabelle

--- src/app/pages/text-area/text-area.component.ts
+++ src/app/pages/text-area/text-area.component.ts
@@ -20,4 +20,37 @@ export class TextAreaComponent {
     document.execCommand(command, false, value);
     this.editor.nativeElement.focus();
   }
+
+  insertNodeAtCaret(node: Node) {
+    const sel = window.getSelection();
+    if (sel && sel.rangeCount) {
+      const range = sel.getRangeAt(0);
+      range.deleteContents();
+      range.insertNode(node);
+    }
+  }
+  createTable() {
+    const table = document.createElement('table');
+    table.setAttribute('style', 'border-collapse: collapse;');
+  
+    const rowsInput = prompt('Inserisci il numero di righe:', '2');
+    const colsInput = prompt('Inserisci il numero di colonne:', '2');
+  
+    const rows = parseInt(rowsInput, 10);
+    const cols = parseInt(colsInput, 10);
+  
+    if (rows && cols) {
+      for (let i = 0; i < rows; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < cols; j++) {
+          const cell = document.createElement('td');
+          cell.innerHTML = '&nbsp;';
+          cell.setAttribute('style', 'border: 1px solid black; padding: 5px;');
+          row.appendChild(cell);
+        }
+        table.appendChild(row);
+      }
+      this.insertNodeAtCaret(table);
+    }
+  }
 }
