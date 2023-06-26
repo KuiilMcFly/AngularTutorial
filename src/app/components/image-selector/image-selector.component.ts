54ae6a510b252bd5788841c7980b3833b0a19793
Vincenzo928el <98588521+Vincenzo928el@users.noreply.github.com>
2023-06-26 09:48:11 +0200

    fix

--- src/app/components/image-selector/image-selector.component.ts
+++ src/app/components/image-selector/image-selector.component.ts
@@ -11,42 +11,52 @@ starImage = 1
     {
      src: 'https://picsum.photos/500/900/?random',
      title: 'Image 1',
+     selected: false
     },
     {
      src: 'https://picsum.photos/450/250/?random',
      title: 'Image 2',
+     selected: false
     },
     {
      src: 'https://picsum.photos/5000/5000/?random',
      title: 'Image 3',
+     selected: false
     },
     {
       src: 'https://picsum.photos/750/450/?random',
       title: 'Image 4',
+      selected: false
      },
      {
       src: 'https://picsum.photos/1500/1000/?random',
       title: 'Image 5',
+      selected: false
      },
      {
       src: 'https://picsum.photos/400/500/?random',
       title: 'Image 6',
+      selected: false
      },
      {
       src: 'https://picsum.photos/600/850/?random',
       title: 'Image 7',
+      selected: false
      },
      {
       src: 'https://picsum.photos/2000/2000/?random',
       title: 'Image 8',
+      selected: false
      },
      {
       src: 'https://picsum.photos/3500/5000/?random',
       title: 'Image 9',
+      selected: false
      },
      {
       src: 'https://picsum.photos/550/100/?random',
       title: 'Image 10',
+      selected: false
      },
   ]
   selectedImages: any[] = [];
